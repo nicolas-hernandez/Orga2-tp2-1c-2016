c4ee90b22bcfe0ec7fe894a5564ac07a43ccbb50
Nicolas <nicoh22@hotmail.com>
2016-04-17 00:12:27 -0300

    Empiezo a escribir el filtro sepia en asm

--- tp2/codigo/filtros/sepia_asm.asm
+++ tp2/codigo/filtros/sepia_asm.asm
@@ -1,6 +1,8 @@
 section .data
 DEFAULT REL
 
+factores: DD  1.0, 0.2, 0.3, 0.5
+
 section .text
 global sepia_asm
 sepia_asm:
@@ -20,10 +22,42 @@ sepia_asm:
 	sar ecx, 2; ecx/4 me muevo cuatro pixeles por iteracion
 
 .ciclo:	
-	movdqu xmm1, [rdi]; XMM1 = | p3 | p2 | p1 | p0 |
+	pxor xmm7, xmm7
+	pxor xmm6, xmm6	
 
-		
-		
+	movdqu xmm1, [rdi]; XMM1 = | p3 | p2 | p1 | p0 |
+	movdqu xmm2, xmm1
+	movdqu xmm5, xmm1; respaldo
+	
+	punpcklbw xmm1, xmm6; XMM1 = | p1 | p0 | 
+	punpckhbw xmm2, xmm7; XMM2 = | p3 | p2 |
+	
+	movdqu xmm3, xmm1
+	movdqu xmm4, xmm2
+	
+	phaddw xmm1, xmm1
+	phaddw xmm1, xmm1
+	phaddw xmm2, xmm2
+	phaddw xmm2, xmm2;  si alfa estuviera en 0 tengo la suma en todos lados
+
+	; un unpack mas, multiplico de a un pixel
+
+	cvtdq2ps xmm1, xmm1
+	cvtdq2ps xmm2, xmm2
+
+	movdqu xmm0, [factores]
+
+	mulps xmm1, xmm0 
+	mulps xmm2, xmm0 
+
+	;pack	
+	
+	;restaurar canal alfa
+
+	;escribir a memoria
+	movdqu [rsi], xmm1
+
+
 	add rdi, 16
 	loop .ciclo	
 	
