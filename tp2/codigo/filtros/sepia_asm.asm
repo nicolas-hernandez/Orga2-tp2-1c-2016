1c203f93a8df7042c9803c34d4c3eb8248e07a7a
Nicolas Carlos Hernandez <nhernandez@ws15.labo5>
2016-04-21 20:40:53 -0300

    Sepia_asm mejor documentado. Mascara fuera de loop

--- tp2/codigo/filtros/sepia_asm.asm
+++ tp2/codigo/filtros/sepia_asm.asm
@@ -34,38 +34,40 @@ sepia_asm:
 	sar ecx, 2; ecx/4 me muevo cuatro pixeles por iteracion
 	
 
+	movdqu xmm7, [alfainv] ; XMM7 = | 00 | FF | FF | FF |...
+	movdqu xmm8, [alfamasc]; XMM8 = | FF | 00 | 00 | 00 |...
+	pxor xmm6, xmm6	
+
 .ciclo:	
 
-	movdqu xmm7, [alfainv] ; XMM7 = | 00 | 00 | 00 | FF |...
-	pxor xmm6, xmm6	
 
 	movdqu xmm1, [rdi]; XMM1 = | p3 | p2 | p1 | p0 |
 	movdqu xmm2, xmm1 ; XMM1 = XMM2
-	movdqu xmm5, xmm1; respaldo
+	movdqu xmm5, xmm1; respaldo XMM5 = XMM1
 
-	;limpiar alfa?
-	pand xmm1, xmm7
-	pand xmm2, xmm7
+	;limpiar alfa
+	pand xmm1, xmm7; XMM1 = | 0 | r3 | g3 | b3 |...
+	pand xmm2, xmm7; idem
 		
 		
-	punpcklbw xmm1, xmm6; XMM1 = | p1 | p0 | 
-	punpckhbw xmm2, xmm6; XMM2 = | p3 | p2 |
+	punpcklbw xmm1, xmm6; XMM1 = | p1 | p0 | con alfa limpio
+	punpckhbw xmm2, xmm6; XMM2 = | p3 | p2 | con alfa limpio
 	
 	movdqu xmm3, xmm1
 	movdqu xmm4, xmm2
-	punpcklbw xmm1, xmm6; XMM1 = |suma0|suma0|suma0|suma0|
-	punpckhbw xmm3, xmm6; XMM3 = |suma1|suma1|suma1|suma1|
-	punpcklbw xmm2, xmm6; XMM2 = |suma2|suma2|suma2|suma2|
-	punpckhbw xmm4, xmm6; XMM4 = |suma3|suma3|suma3|suma3|
+	punpcklbw xmm1, xmm6; XMM1 = | 0 | r0 | g0 | b0 |
+	punpckhbw xmm3, xmm6; XMM3 = | 0 | r1 | g1 | b1 |
+	punpcklbw xmm2, xmm6; XMM2 = | 0 | r2 | g2 | b2 |
+	punpckhbw xmm4, xmm6; XMM4 = | 0 | r3 | g3 | b3 |
 	
-	phaddd xmm1, xmm1; XMM1 = |r1+a1|b1+g1|r1+a1|b1+g1|r0+a0|b0+g0|r0+a0|b0+g0|
-	phaddd xmm1, xmm1; XMM1 = |suma1|suma1|suma1|suma1|suma0|suma0|suma0|suma0|
-	phaddd xmm2, xmm2; idem con pixeles 2 y 3
+	phaddd xmm1, xmm1; XMM1 = | r0 | g0 + b0 | r0 | g0 + b0 |
+	phaddd xmm1, xmm1; XMM1 = | suma0 | suma0 | suma0 | suma0 | 
+	phaddd xmm2, xmm2; idem con pixeles 1, 2 y 3
 	phaddd xmm2, xmm2
-	phaddd xmm3, xmm3; idem con pixeles 2 y 3
-	phaddd xmm3, xmm3; idem con pixeles 2 y 3
-	phaddd xmm4, xmm4; idem con pixeles 2 y 3
-	phaddd xmm4, xmm4; idem con pixeles 2 y 3
+	phaddd xmm3, xmm3; 
+	phaddd xmm3, xmm3;
+	phaddd xmm4, xmm4;
+	phaddd xmm4, xmm4;
 
 	; un unpack mas, multiplico de a un pixel
 
@@ -76,24 +78,23 @@ sepia_asm:
 
 	movups xmm0, [factores]
 
-	mulps xmm1, xmm0; XMM1 = |suma0*0.5|suma0*0.3|suma0*0.2|*|  
-	mulps xmm2, xmm0; XMM2 = |suma2*0.5|suma2*0.3|suma2*0.2|*|  
-	mulps xmm3, xmm0; XMM3 = |suma1*0.5|suma1*0.3|suma1*0.2|*|   
-	mulps xmm4, xmm0; XMM4 = |suma3*0.5|suma3*0.3|suma3*0.2|*|   
+	mulps xmm1, xmm0; XMM1 = |*|suma0*0.2|suma0*0.3|suma0*0.5|  
+	mulps xmm2, xmm0; XMM2 = |*|suma2*0.2|suma2*0.3|suma2*0.5|  
+	mulps xmm3, xmm0; XMM3 = |*|suma1*0.2|suma1*0.3|suma1*0.5|   
+	mulps xmm4, xmm0; XMM4 = |*|suma3*0.2|suma3*0.3|suma3*0.5|   
 
-	cvttps2dq xmm1, xmm1; |suma0*0.5 asInt|suma0*0.3 asInt|suma0*0.2 asInt|*| 
+	cvttps2dq xmm1, xmm1; |0|suma0*0.2 asInt|suma0*0.3 asInt|suma0*0.5 asInt|
 	cvttps2dq xmm2, xmm2
 	cvttps2dq xmm3, xmm3
 	cvttps2dq xmm4, xmm4
 
-	packusdw xmm1, xmm3
-	packusdw xmm2, xmm4
+	packusdw xmm1, xmm3; 
+	packusdw xmm2, xmm4;
 	packuswb xmm1, xmm2; XMM1 = | p3' | p2' | p1'| p0'|
 	
 
 	;restaurar canal alfa
-	movdqu xmm7, [alfamasc]
-	pand xmm5, xmm7
+	pand xmm5, xmm8
 	paddb xmm1, xmm5
 
 	;escribir a memoria
