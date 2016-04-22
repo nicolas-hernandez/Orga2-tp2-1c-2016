52b93c4f316215b8bc129ee17fc2808bdafbd028
Sr.Luchox <luchiox@users.noreply.github.com>
2016-04-21 21:42:58 -0300

    Update cropflip_asm.asm

--- tp2/codigo/filtros/cropflip_asm.asm
+++ tp2/codigo/filtros/cropflip_asm.asm
@@ -1,5 +1,4 @@
 global cropflip_asm
-section .data
 %define MASK_INV 00011011
 section .text
 ;void cropflip_asm(unsigned char *src,
@@ -22,7 +21,6 @@ section .text
 cropflip_asm:
     push rbp
     mov rbp, rsp
-    sub rsp, 8
     push rbx
     push rcx
     push r12
@@ -35,46 +33,71 @@ cropflip_asm:
     xor r14, r14
     xor r10, r10
     xor rcx, rcx
-   
-    mov r14d, [rsp + 4*4]	; mov r14b, tamx (cuantos pixeles en x me piden)
-    mov rax, r14
-    mul 4
-    mov r9, eax             ; Es lo q devuelvo(ancho de fila en bytes)    
+    xor r15, r15
+    xor rbx, rbx
+
+	mov r14d, [rbp + 16]	 			;(cuantos pixeles en x me piden)                     			
     
-    mov r13d, [rsp + 3*4]	; mov r13b, tamy (cuantos pixeles en y me piden)
+    mov r13d, r14d
+    shl r13, 2
+    mov r9d, r13d						; Es lo q devuelvo(ancho de fila en bytes)
 
-    mov ebx, [rsp + 2*4] 	; mov rbx, offsetx
-    lea rbx, [rdi + ebx * 4]	; Pos de memoria de x desde donde tengo q copiar
+    mov r13d, [rbp + 24]	; mov r13b, tamy (cuantos pixeles en y me piden)
+    
+    mov r12d, [rbp + 32] 	; mov r12, offsetx			    
+							    
+	mov ebx, [rbp + 40]
+							 
+	mov r15d, r13d 
 
-    mov r10d, [rsp + 1*4]	; mov r10, offsety
-    lea rbx, [rbx + r10d * r8d]	; Pos de memoria (x,y) desde donde tengo q copiar
- 
-    mov r15, rbx            	; Copio rbx en r15 para no perderlo
+.preC:
+	mov ecx, r14d
+	shr rcx, 2
+.c:
+	lea rsi, [rsi + 16]
+	loop .c
+	sub r15d, 1	 		    
+    cmp r15d, 0  	        
+    jne .preC
+
+
+    shl r12, 2
+    lea rdi, [rdi + r12]
+    shl ebx, 2
+    mov ecx, ebx
+
+    mov r15d, r8d
+    shr r15, 2
+
+    .sumarDir:				;usaria "mul r8d" pero por alguna razon no anda
+    lea rdi, [rdi + r15]
+    loop .sumarDir
+
+    shl r15, 2
+    mov ecx, r14d
+    shl rcx, 2
+    sub r15d, ecx
 
 .preCiclo:
-    mov rax, r14
-    mul 4
-    mul r13
-    mov r12, rax
-    lea r12, [rsi + r12]
-    lea r12, [r12 - 16] 		    
-	
-    mov ecx, r14	    	; Guardo en ecx la cantidad de pixeles a copiar 
+					    
+    mov ecx, r14d			; Guardo en ecx la cantidad de pixeles a copiar 
     shr rcx, 2        		; Obtengo de a 4 pixeles
     
 .ciclo:
-    movdqu xmm1, [r15] 						;p0|p1|p2|p3
-    pshufd xmm7, xmm1,  MASK_INV ;lo doy vuelta (shuffle) 	;p3|p2|p1|p0
+    movdqu xmm1, [rdi] 										;p0|p1|p2|p3
+    pshufd xmm7, xmm1,  MASK_INV ;lo doy vuelta (shuffle) 	 p3|p2|p1|p0
         
-    movdqu [r12], xmm1 		;copio en la matriz en la pos r12 los pixeles de xmm1 directamente
+    movdqu [rsi], xmm7 		;copio en la matriz en la pos r12 los pixeles de xmm1 directamente
     
-    sub r12, 16 		    ;bajo la pos de la fila para guardar en dst
-    add r15, 16 		    ;adelanto a los proximos 4 pixeles
+    lea rsi, [rsi - 16] 		    ;bajo la pos de la fila para guardar en dst
+    lea rdi, [rdi + 16] 		    ;adelanto a los proximos 4 pixeles
     loop .ciclo
     
+
+    lea rdi, [rdi + r15]
     sub r13d, 1	 		    ;descuento una fila
-    cmp r13d, 0  	        ;comparo con la pos y de partida
-    jge .preCiclo
+    cmp r13d, 0 	        ;comparo con la pos y de partida
+    jne .preCiclo
     
     pop r15
     pop r14
@@ -82,6 +105,5 @@ cropflip_asm:
     pop r12
     pop rcx
     pop rbx
-    add rsp, 8
     pop rbp
     ret
