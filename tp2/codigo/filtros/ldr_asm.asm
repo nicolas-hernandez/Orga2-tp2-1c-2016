1006834c8fa04853aa2f547dcaafa916973f1941
rkapobel01 <rok_35@live.com.ar>
2016-04-26 00:43:36 -0300

    refactor source ldr asm

--- tp2/codigo/filtros/ldr_asm.asm
+++ tp2/codigo/filtros/ldr_asm.asm
@@ -9,8 +9,8 @@ DEFAULT REL
 ; En memoria: BGRA
 ; En registros: ARGB
 ; a3|r3|g3|b3|a2|r2|g2|b2|a1|r1|g1|b1|a0|r0|g0|b0 -> a3|a2|a1|a0|r3|g3|b3|r2|g2|b2|r1|g1|b1|r0|g0|b0
-ordenarCanalesPixelesImparesAWord: DB 0x00, 0x83, 0x01, 0x84, 0x02, 0x85, 0x86, 0x87, 0x08, 0x8B, 0x09, 0x8C, 0x0A, 0x8D, 0x8E, 0x8F 
-ordenarCanalesPixelesParesAWord: DB 0x04, 0x80, 0x05, 0x81, 0x06, 0x82, 0x83, 0x87, 0x0C, 0x88, 0x0D, 0x89, 0x0E, 0x8A, 0x8B, 0x8F 
+ordenarCanalesPixelesParesAWord: DB 0x00, 0x83, 0x01, 0x84, 0x02, 0x85, 0x86, 0x87, 0x08, 0x8B, 0x09, 0x8C, 0x0A, 0x8D, 0x8E, 0x8F 
+ordenarCanalesPixelesImparesAWord: DB 0x04, 0x80, 0x05, 0x81, 0x06, 0x82, 0x83, 0x87, 0x0C, 0x88, 0x0D, 0x89, 0x0E, 0x8A, 0x8B, 0x8F 
 ordenarADoubleWord: DB 0x00, 0x01, 0x88, 0x89, 0x2, 0x03, 0x8A, 0x8B, 0x04, 0x05, 0x8C, 0x8D, 0x06, 0x07, 0x8E, 0x8F
 salvarUnPixelShifteable: DB 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x00
 maxValue: DD 0x004A6A4B ; check this 4876875
@@ -82,12 +82,16 @@ ldr_asm:
 
 	shl r8, 1 ; r8*2 = i = 2 - j = 0
 
-	movdqu xmm6, [ordenarCanalesPixelesImparesAWord]
-	movdqu xmm7, [ordenarCanalesPixelesParesAWord]
+	movdqu xmm6, [ordenarCanalesPixelesParesAWord]
+	movdqu xmm7, [ordenarCanalesPixelesImparesAWord]
 	movdqu xmm8, [salvarUnPixelShifteable]
 	movdqu xmm11, [ordenarADoubleWord]
 	movdqu xmm15, xmm8 ; 0|FF|FF|FF|0|0|0|0|0|0|0|0|0|0|0|0
     psrldq xmm15, 12 ; 0|0|0|0|0|0|0|0|0|0|0|0|0|FF|FF|FF
+    movdqu xmm4, xmm8
+    psrldq xmm4, 14
+    pslldq xmm4, 3 ; 0|0|0|0|0|0|0|0|0|0|0|0|F|0|0|0
+    
 
 .ciclo: ; while(r8 < rcx) == (actual < total) 
 ; if(j > 1)
@@ -110,37 +114,37 @@ ldr_asm:
 
 	movdqu xmm13, [rdi + r12*4] ; Li3|Li2|Li1|Li0
 	movdqu xmm9, xmm13
-	pshufb xmm9, xmm6 ; ordenarCanalesPixelesImparesAWord
-	phaddw xmm9, xmm10 ; maximo por dw = 510 = 0x01FE
-	pshufb xmm13, xmm7 ; ordenarCanalesPixelesParesAWord
-    phaddw xmm13, xmm10 ; maximo por dw = 510 = 0x01FE
-    pshufb xmm9, xmm11 ; ordenarADoubleWord
-    pshufb xmm13, xmm11 ; ordenarADoubleWord
-    cvtdq2ps xmm9, xmm9
-    cvtdq2ps xmm13, xmm13
-    addps xmm13, xmm9 ; cuatro fp sumados
-    movdqu xmm9, xmm13 ; shifteo dos fp de la parte alta a la parte baja
-    psrldq xmm9, 8
-    addps xmm13, xmm9 ; dos fp en la parte baja 
-    pslldq xmm13, 8
-    psrldq xmm13, 8 ; limpio parte alta
+	pshufb xmm9, xmm6 ; 0|0|0|r2|0|g2|0|b2|0|0|0|r0|0|g0|0|b0
+	phaddw xmm9, xmm10 ; 0|0|0|0|0+r2|g2+b2|0+r0|g0+b0 maximo por dw = 510 = 0x01FE
+	pshufb xmm13, xmm7 ; 0|0|0|r3|0|g3|0|b3|0|0|0|r1|0|g1|0|b1
+    phaddw xmm13, xmm10 ; 0|0|0|0|r3|g3+b3|r1|g1+b1 maximo por dw = 510 = 0x01FE
+    pshufb xmm9, xmm11 ; 0|r2|0|g2+b2|0|r0|0|g0+b0
+    pshufb xmm13, xmm11 ; r3|g3+b3|r1|g1+b1
+    cvtdq2ps xmm9, xmm9 ; fp(r2)|fp(g2+b2)|fp(0+r0)|fp(g0+b0)
+    cvtdq2ps xmm13, xmm13 ; fp(r3)|fp(g3+b3)|fp(0+r1)|fp(g1+b1)
+    addps xmm13, xmm9 ; fp(r3+0+r2)|fp(g3+b3+g2+b2)|fp(0+r1+r0)|fp(g1+b1+g0+b0)
+    movdqu xmm9, xmm13
+    psrldq xmm9, 8 ; 0|0|fp(0+r3+0+r2)|fp(g3+b3+g2+b2)
+    addps xmm13, xmm9 ; fp(r3+r2)|fp(g3+b3+g2+b2)|fp(0+r1+0+r0+r3+0+r2)|fp(g1+b1+g0+b0+g3+b3+g2+b2)
+    pslldq xmm13, 8 ; fp(r1+r0+r3+r2)|fp(g1+b1+g0+b0+g3+b3+g2+b2)|0|0
+    psrldq xmm13, 8 ; 0|0|fp(r1+r0+r3+r2)|fp(g1+b1+g0+b0+g3+b3+g2+b2)
     
 	movd xmm9, [rdi + r12*4 + 16] ; 0|0|0|0|0|0|0|0|0|0|0|0|a4|r4|g4|b4
 
 	pslldq xmm9, 12 ; a4|r4|g4|b4|0|0|0|0|0|0|0|0|0|0|0|0
 	pand xmm9, xmm8 ; 0|r4|g4|b4|0|0|0|0|0|0|0|0|0|0|0|0
-	pshufb xmm9, xmm7 ; ordenarCanalesPixelesParesAWord
-	phaddw xmm9, xmm10
-	pshufb xmm9, xmm11 ; ordenarADoubleWord
-	cvtdq2ps xmm9, xmm9 ; dos fp en la parte alta
-	psrldq xmm9, 8
-	addps xmm13, xmm9 ; dos fp en la parte baja 
+	pshufb xmm9, xmm7 ; 0|0|0|r4|0|g4|0|b4|0|0|0|0|0|0|0|0
+	phaddw xmm9, xmm10 ; 0|0|0|0|r4|g4+b4|0|0 maximo por dw = 510 = 0x01FE
+	pshufb xmm9, xmm11 ; 0|r4|0|g4+b4|0|0|0|0
+	cvtdq2ps xmm9, xmm9 ; fp(r4)|fp(g4+b4)|0|0
+	psrldq xmm9, 8 ; 0|0|fp(r4)|fp(g4+b4)
+	addps xmm13, xmm9 ; 0|0|fp(r1+r0+r3+0+r2+r4)|fp(g1+b1+g0+b0+g3+b3+g2+b2+g4+b4)
 	movdqu xmm9, xmm13
-	psrldq xmm9, 4
-	addps xmm13, xmm9 ; un fp en la parte baja
-	pslldq xmm13, 12
-	psrldq xmm13, 12
-	addps xmm0, xmm13 ; suma de la i fila para el pixel index 4.
+	psrldq xmm9, 4 ; 0|0|0|fp(r1+r0+r3+0+r2+r4)
+	addps xmm13, xmm9 ; 0|0|fp(r1+r0+r3+0+r2+r4)|fp(g1+b1+g0+b0+g3+b3+g2+b2+g4+b4+r1+r0+r3+0+r2+r4)
+	pslldq xmm13, 12 ; fp(g1+b1+g0+b0+g3+b3+g2+b2+g4+b4+r1+r0+r3+0+r2+r4)|0|0|0
+	psrldq xmm13, 12 ; 0|0|0|fp(g1+b1+g0+b0+g3+b3+g2+b2+g4+b4+r1+r0+r3+0+r2+r4)
+	addps xmm0, xmm13 ; suma de la i fila para el pixel ij.
 
 	add r12, r15
 	inc r10
@@ -165,36 +169,30 @@ ldr_asm:
 	pslldq xmm13, 4 ; max|max|max|0
 	por xmm13, xmm14 ; max|max|max|max
 
-	cmp r9, r11
-	jge .mayorIgAColsToProccess ; mayor igual a colsToProccess
-
-    psrldq xmm8, 14
-    pslldq xmm8, 3 ; 0|0|0|0|0|0|0|0|0|0|0|0|F|0|0|0
-
 	movdqu xmm5, xmm0
 	pslldq xmm5, 4
 	por xmm5, xmm0
 	pslldq xmm5, 4
 	por xmm5, xmm0
 	pxor xmm14, xmm14
-	movd xmm14, [rdi + r8*4] ; 0|0|0|0|0|0|0|0|0|0|0|0|aj|rj|gj|bj <- get pixel ij
-	movdqu xmm0, xmm14 ; 0|0|0|0|0|0|0|0|0|0|0|0|aj|rj|gj|bj
-	pand xmm0, xmm15 ; 0|0|0|0|0|0|0|0|0|0|0|0|0|rj|gj|bj
+	movd xmm14, [rdi + r8*4] ; 0|0|0|0|0|0|0|0|0|0|0|0|a|r|g|b <- get pixel ij
+	movdqu xmm0, xmm14 ; 0|0|0|0|0|0|0|0|0|0|0|0|a|r|g|b
+	pand xmm0, xmm15 ; 0|0|0|0|0|0|0|0|0|0|0|0|0|r|g|b
 	pxor xmm10, xmm10
-	punpcklbw xmm0, xmm10 ; 0|0|0|0|0|rj|gj|bj
-	punpcklwd xmm0, xmm10 ; 0|rj|gj|bj
+	punpcklbw xmm0, xmm10 ; 0|0|0|0|0|r|g|b
+	punpcklwd xmm0, xmm10 ; 0|r|g|b
 	cvtdq2ps xmm0, xmm0 ; cast to float!
-	mulps xmm5, xmm0 ; 0|sumargb_i,j*rj|sumargb_i,j*gj|sumargb_i,j*bj
-	mulps xmm5, xmm12 ; 0|alpha*sumargb_i,j*rj|alpha*sumargb_i,j*gj|alpha*sumargb_i,j*bj <- puede cambiar el signo segun alpha.
-	divps xmm5, xmm13 ; 0|(alpha*sumargb_i,j*rj)/max|(alpha*sumargb_i,j*gj)/max|(alpha*sumargb_i,j*bj)/max
-	addps xmm5, xmm0 ; 0|rj+(alpha*sumargb_i,j*rj)/max|gj+(alpha*sumargb_i,j*gj)/max|bj+(alpha*sumargb_i,j*bj)/max
+	mulps xmm5, xmm0 ; 0|sumargb*r|sumargb*g|sumargb*b
+	mulps xmm5, xmm12 ; 0|alpha*sumargb*r|alpha*sumargb*g|alpha*sumargb*b <- puede cambiar el signo segun alpha.
+	divps xmm5, xmm13 ; 0|(alpha*sumargb*r)/max|(alpha*sumargb*g)/max|(alpha*sumargb*b)/max
+	addps xmm5, xmm0 ; 0|r+(alpha*sumargb*r)/max|g+(alpha*sumargb*g)/max|b+(alpha*sumargb*b)/max
 	
 	cvttps2dq xmm5, xmm5 ; cast to dw signed 
 	pxor xmm10, xmm10
-	packusdw xmm5, xmm10 ; 0|0|0|0|0|rj+(alpha*sumargb_i,j*rj)/gj+max|(alpha*sumargb_i,j*gj)/bj+max|(alpha*sumargb_i,j*bj)/max
-	packuswb xmm5, xmm10 ; 0|0|0|0|0|0|0|0|0|0|0|0|0|rj+(alpha*sumargb_i,j*rj)/gj+max|(alpha*sumargb_i,j*gj)/bj+max|(alpha*sumargb_i,j*bj)/max <- tengo los canales calculados saturados a byte.
-	pand xmm14, xmm8 ; 0|0|0|0|0|0|0|0|0|0|0|0|aj|0|0|0
-	por xmm14, xmm5 ; 0|0|0|0|0|0|0|0|0|0|0|0|aj|rj+(alpha*sumargb_i,j*rj)/max|gj+(alpha*sumargb_i,j*gj)/max|bj+(alpha*sumargb_i,j*bj)/max
+	packusdw xmm5, xmm10 ; 0|0|0|0|0|r+(alpha*sumargb*r)/g+max|(alpha*sumargb*g)/b+max|(alpha*sumargb*b)/max
+	packuswb xmm5, xmm10 ; 0|0|0|0|0|0|0|0|0|0|0|0|0|r+(alpha*sumargb*r)/g+max|(alpha*sumargb*g)/b+max|(alpha*sumargb*b)/max <- tengo los canales calculados saturados a byte.
+	pand xmm14, xmm4 ; 0|0|0|0|0|0|0|0|0|0|0|0|a|0|0|0
+	por xmm14, xmm5 ; 0|0|0|0|0|0|0|0|0|0|0|0|a|r+(alpha*sumargb*r)/max|g+(alpha*sumargb*g)/max|b+(alpha*sumargb*b)/max
 
     movd [rsi + r8*4], xmm14
 
