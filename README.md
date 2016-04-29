959f3639eb4dd806f04d24d0bc53e302045626f1
Nicolas <nicoh22@hotmail.com>
2016-04-29 10:09:15 -0300

    tp2.sh casi version final. Readme actualizado con modo de uso

--- README.md
+++ README.md
@@ -8,3 +8,22 @@ Estaria bueno ir poniendo aca tips, notas y comandos que queremos que
 el resto del grupo vea; por ejemplo uso de ciertos scripts para generar
 graficos, uso/sintaxis de paquetes de latex o cosas por el estilo.
 
+
+Uso de tp2.sh:
+
+	./tp2.sh MODO FILTRO IMAGEN [opcionesFiltro]
+
+	MODO:
+		asm: corre el make comun y la implementacion asm
+		c0: corre el make con el flag -O0 (sin ninguna optimizacion) y luego el tp2 con "-i c"
+		c1: corre el make con el flag -O1 (el default de gcc)
+		c2: corre el make con el flag -O2 (las optimizaciones del nivel anterior y algunas mas)
+		c3: corre el make con el flag -O3 (todas las optimizaciones. creo)
+	
+	FILTRO: ldr cropflip sepia
+	IMAGEN: path relativo de la imagen ("codigo/img/scarlett.bmp" funca pero "/home/nico/scarlett.bmp" no)
+
+	Ojo, no esta pensado para correrse desde cualquier directorio.
+
+	Los resultados se guardan en codigo/log.txt. El script sobrescribe este archivo.
+
--- tp2/tp2.sh
+++ tp2/tp2.sh
@@ -1,16 +1,37 @@
 MODE=$1
 FILTER=$2
+PIC=$3
 FILTER_ARGS=""
 BUILD_DIR="codigo/"
 PROGRAM="build/tp2"
-TEST_PIC="img/lena32.bmp"
 LOGFILE="log.txt"
 
+rm $BUILD_DIR$LOGFILE
+
 function usage {
-    echo "Should be implemented."
+	echo "Modo de uso:"
+	echo "./tp2.sh modo filtro imagen [opcionesFiltro]"
+	echo "	Donde modo es: asm c0 c1 c2 c3."
+	echo "	modo determina que implementacion del filtro se ejecuta"
+	echo "	Filtro : sepia cropflip ldr"
 }
 
 #TODO: checkear el contenido de filter y llenar filter args
+case $FILTER in
+	sepia)
+		;;
+	ldr)
+		FILTER_ARGS=$4
+		#Si queres pasar negativos van entre comillas
+		#ej: "-- -20"
+		;;
+	cropflip)
+		FILTER_ARGS="$4 $5 $6 $7"
+		;;
+	*)
+		usage
+		exit 1
+esac
 
 case $MODE in
     asm)
@@ -37,13 +58,16 @@ case $MODE in
         exit 1
 esac
 
+WORK_DIR=$(pwd)
+TEST_PIC=$WORK_DIR/$PIC
+
 cd $BUILD_DIR
 make $BUILD_ARGS
 let TIMES=100
 typeset -i i TIMES 
 for ((i=1;i<=TIMES;++i)); do
 	./$PROGRAM $OPTIONS $FILTER $TEST_PIC $FILTER_ARGS \
-		| grep llamada >> $LOGFILE
+		>> $LOGFILE
 done
 
 
