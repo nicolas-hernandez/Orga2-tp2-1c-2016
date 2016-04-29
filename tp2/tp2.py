a500ac2eaecc92f7a89c21ac109264cc695e6ec3
rkapobel01 <rok_35@live.com.ar>
2016-04-29 17:48:47 -0300

    forro

--- tp2/tp2.py
+++ tp2/tp2.py
@@ -1,44 +1,106 @@
 #!/usr/bin/python
-#from scripts.fabricate import *
-#from scripts.settings import *
-#from scripts.utils import listfiles
+# from scripts.fabricate import *
+# from scripts.settings import *
+# from scripts.utils import listfiles
 import os
-from sys import argv
+import getopt
+import sys
+from settings import Options, Tests
 
-# Acciones
-def build():
-  compile()
 
-def compile():
+def build(option):
     build_dir = "codigo/"
-    cwd = os.getcwd() # get current directory
+    cwd = os.getcwd()  # get current directory
     os.chdir(build_dir)
-    os.system("make -e CFLAGS64=\"-O3 -g -ggdb -Wall -Wextra -std=c99 -pedantic -m64\" ")
-    #os.system("make")
-    os.chdir(cwd)
 
-#    for source in sources:
-#    run(compiler, '-c', source+'.cpp', '-o', source+'.o')
+    flags = ""
+
+    if option == Options.o1:
+        flags = '-e CFLAGS64="-O1 -g -ggdb -Wall -Wextra -std=c99 -pedantic -m64"'
+    elif option == Options.o2:
+        flags = '-e CFLAGS64="-O2 -g -ggdb -Wall -Wextra -std=c99 -pedantic -m64"'
+    elif option == Options.o3:
+        flags = '-e CFLAGS64="-O3 -g -ggdb -Wall -Wextra -std=c99 -pedantic -m64"'
+
+    command = "make" + flags
+    os.system(command)
+    os.chdir(cwd)
 
 
 def clean():
     build_dir = "codigo/"
-    cwd = os.getcwd() # get current directory
+    cwd = os.getcwd()  # get current directory
     os.chdir(build_dir)
     os.system("make clean")
     os.chdir(cwd)
 
-def test():
-  build()
-  import unittest
-  unittest.main(module='scripts.tptests', exit=False, argv=argv[:1], verbosity=3)
 
-def main():
-    #build()
-    #test()
-    clean()
-    
+# def test(test, graficar):
+# import unittest
+# unittest.main(module='scripts.tptests', exit=False, argv=argv[:1], verbosity=3)
 
-main()
+def printAllInfo():
+    print "tp2.py -h <help> -f <flag> -i <inputfile> -o <outputfile> -t <test> -g <graficar> \n"
 
-    
+def main(argv):
+    inputfile = ""
+    outputfile = ""
+    flag = ""
+    # test = ""
+    # graficar = False
+
+    try:
+        opts, args = getopt.getopt(argv, "hfi:o:t:g:", ["help=", "flag=", "ifile=", "ofile=", "test="])
+    except getopt.GetoptError:
+        printAllInfo()
+        sys.exit(2)
+    if len(opts) == 0:
+        printAllInfo()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            printAllInfo()
+            Options.printAllInfo()
+            Tests.printAllInfo()
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            if arg == "":
+                arg = "in.txt"
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            if arg == "":
+                arg = "out.txt"
+            outputfile = arg
+        elif opt in ("-f", "--flag"):
+            if arg not in (Options.none, Options.o, Options.o1, Options.o2, Options.o3):
+                Options.printAllInfo()
+                sys.exit(2)
+            flag = arg
+        elif opt in ("-t", "--test"):
+            if arg not in (Tests.cacheCropflip, Tests.clocksCf, Tests.clocksLdr, Tests.clocksSep, Tests.tamaniosCf,
+                           Tests.tamaniosLdr, Tests.tamaniosSep, Tests.compareLdrA, Tests.compareLdrB):
+                Tests.printAllInfo()
+                sys.exit(2)
+                # test = arg
+                # elif opt in ("-g"):
+                # graficar = True
+        else:
+            printAllInfo()
+            Options.printAllInfo()
+            Tests.printAllInfo()
+            sys.exit(2)
+
+    print "Input file is ", inputfile
+    print "Output file is ", outputfile
+    print "Flag is ", flag
+
+    if flag != Options.none:
+        clean()
+        build(flag)
+
+        #test(test, graficar)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+else:
+    print("tp2.py is being imported into another module")
