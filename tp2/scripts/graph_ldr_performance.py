6bfd454c0fbadb9ad9fb2ea112c45f3d90a732c0
rkapobel01 <rok_35@live.com.ar>
2016-05-01 21:43:03 -0300

    changing graph ldr

--- tp2/scripts/graph_ldr_performance.py
+++ tp2/scripts/graph_ldr_performance.py
@@ -24,19 +24,20 @@ def graph(test):
                elif row[0] == "asm":
                   meanAsm = float(row[1])
 
-    ind = np.arange(1)  # the x locations for the groups
-    width = 0.5 # the width of the bars
+    ind = np.arange(3)  # the x locations for the groups
+    width = 0.40 # the width of the bars
 
     fig, ax = plt.subplots()
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0)) # for both axis use both
-    rects1 = ax.bar(ind, meanAsm, width, color='r')
-    rects2 = ax.bar(ind + width, meanC, width, color='b')
+    rects1 = ax.bar(ind, [0, meanAsm, 0], width, color='r')
+    rects2 = ax.bar(ind + width, [0, meanC, 0], width, color='b')
 
     # add some text for labels, title and axes ticks
     ax.set_ylabel('Clocks por insumidos')
     ax.set_title('Implementacion')
     ax.set_xticks(ind + width)
-    ax.set_xticklabels(('Asm', 'C'))
+    ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 0)) # for both axis use both
+    # ax.set_xticklabels(('G1', 'G2')) if you have groups
 
     ax.legend((rects1[0], rects2[0]), ('Asm', 'C'))
 
