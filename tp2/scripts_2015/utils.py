c33dc78211dfa011f20ab3b3d2d35c22116a1760
rkapobel01 <rok_35@live.com.ar>
2016-05-02 12:50:11 -0300

    mugre

--- tp2/scripts_2015/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-from fnmatch import fnmatch
-
-def listfiles(root, pattern):
-  for path, subdirs, files in os.walk(root):
-    for name in files:
-      if fnmatch(name, pattern):
-        yield os.path.join(path, name)
-
